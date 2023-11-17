ffd6b459975ea9751455aff364deea79ffc83fe0
fawad ali <fawad1824@gmail.com>
2023-11-17 17:00:45 +0500

    update port

--- server.js
+++ server.js
@@ -7,39 +7,44 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(cors()); // Enable CORS for all routes
+// Enable CORS for all routes
+app.use(cors({
+    origin: '*',  // Replace with the actual URL of your React app
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+}));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('A user connected');
+    console.log('A user connected');
 
-  socket.on('offer', (offer) => {
-    socket.broadcast.emit('offer', offer);
-  });
+    socket.on('offer', (offer) => {
+        socket.broadcast.emit('offer', offer);
+    });
 
-  socket.on('answer', (answer) => {
-    socket.broadcast.emit('answer', answer);
-  });
+    socket.on('answer', (answer) => {
+        socket.broadcast.emit('answer', answer);
+    });
 
-  socket.on('ice-candidate', (candidate) => {
-    socket.broadcast.emit('ice-candidate', candidate);
-  });
+    socket.on('ice-candidate', (candidate) => {
+        socket.broadcast.emit('ice-candidate', candidate);
+    });
 
-  // Listen for 'message' from a peer and broadcast it to all other peers
-  socket.on('message', (message) => {
-    console.log(message);
-    socket.broadcast.emit('message', message);
-  });
+    // Listen for 'message' from a peer and broadcast it to all other peers
+    socket.on('message', (message) => {
+        console.log(message);
+        socket.broadcast.emit('message', message);
+    });
 
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
 });
 
 const PORT = 8080;
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
