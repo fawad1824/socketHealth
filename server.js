aecab78e12ee67533dd69838509d7caa4fd3cb12
fawad ali <fawad1824@gmail.com>
2023-11-17 16:46:25 +0500

    update

--- server.js
+++ server.js
@@ -6,26 +6,37 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
 io.on('connection', (socket) => {
-    console.log('Socket Connected');
+  console.log('A user connected');
 
-    socket.on('offer', (data) => {
-        socket.broadcast.emit('offer', data);
-    });
-    socket.on('answer', (data) => {
-        socket.broadcast.emit('answer', data);
-    });
-    socket.on('ice-candidate', (data) => {
-        socket.broadcast.emit('ice-candidate', data);
-    });
+  socket.on('offer', (offer) => {
+    socket.broadcast.emit('offer', offer);
+  });
 
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
+  socket.on('answer', (answer) => {
+    socket.broadcast.emit('answer', answer);
+  });
+
+  socket.on('ice-candidate', (candidate) => {
+    socket.broadcast.emit('ice-candidate', candidate);
+  });
+
+  // Listen for 'message' from a peer and broadcast it to all other peers
+  socket.on('message', (message) => {
+    console.log(message);
+    socket.broadcast.emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
 });
 
 const PORT = 3000;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
